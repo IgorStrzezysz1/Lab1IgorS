e3c8325b212e8b038bfde933be3d4bd73e3016c8
igor Strzeżysz <igor.strzezysz@wp.pl>
2024-03-13 11:25:23 +0100

    Frsit Commit

---
+++ Zarowka.java
@@ -0,0 +1,30 @@
+
+public class Zarowka {
+    private boolean czySwieci;
+    private boolean czySpalona = false;
+
+    public void zapal() {
+        if (!czySpalona) {
+            czySwieci = true;
+        }
+    }
+
+    public void zgas() {
+        if (!czySpalona) {
+            czySwieci = false;
+        }
+    }
+
+    public boolean sprawdzCzySwieci() {
+        return czySwieci;
+    }
+
+    public boolean sprawdzCzySpalona() {
+        return czySpalona;
+    }
+
+    public void spal() {
+        czySwieci = false;
+        czySpalona = true;
+    }
+}
